6bbb2763c33357b2e7fdcaafe2e557b7d8b23c9f
Claudia Tax <claudiatax05@gmail.com>
2026-02-06 12:17:28 -0600

    se creo una logica para que funcione el formulario del index

---
+++ logica.js
@@ -0,0 +1,18 @@
+        const form = document.getElementById("formNombre");
+        const input = document.getElementById("nombreInput");
+        const lista = document.getElementById("listaNombres");
+
+        form.addEventListener("submit", function(event) {
+            event.preventDefault(); 
+
+            const nombre = input.value.trim();
+
+            if (nombre !== "") {
+                const li = document.createElement("li");
+                li.textContent = nombre;
+                lista.appendChild(li);
+
+                input.value = ""; // limpia el input
+                input.focus();    // vuelve a enfocar
+            }
+        });
